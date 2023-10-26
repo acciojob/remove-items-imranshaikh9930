bdc6a20ea5c13860da2f6f85713abebb3eaf0352
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-26 13:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const inputbtn = document.querySelector("input");
+const colorSelect = document.querySelector("#colorSelect");
+const option = document.querySelectorAll("option");
+
+
+
+inputbtn.addEventListener("click",()=>{
+  let updatedList = option.forEach((op)=>{
+
+    if(colorSelect.value === op.textContent){
+
+      colorSelect.removeChild(op);
+
+    }
+  });
+
+  // console.log(colorSelect.value);
+
+})
