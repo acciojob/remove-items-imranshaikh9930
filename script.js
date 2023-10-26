aeff7cb73702c0ba2b983daa53dbed5ef9c16508
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-26 13:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,10 @@ const inputbtn = document.querySelector("input");
 const colorSelect = document.querySelector("#colorSelect");
 const option = document.querySelectorAll("option");
 
-
-
-inputbtn.addEventListener("click",()=>{
-  let updatedList = option.forEach((op)=>{
-
-    if(colorSelect.value === op.textContent){
-
-      colorSelect.removeChild(op);
-
-    }
-  });
-
-  // console.log(colorSelect.value);
-
-})
+inputbtn.addEventListener("click", () => {
+    option.forEach((op) => {
+        if (colorSelect.value === op.textContent) {
+            colorSelect.removeChild(op);
+        }
+    });
+});
